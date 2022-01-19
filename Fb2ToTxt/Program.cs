409c23d60821f4f7a0b30afb07f1002b8b5d6531
DmitryGolubenkov <xxdimonxx15@yandex.ru>
2022-01-19 15:42:38 +0300

    Обнаружение архивов, сохранение в папке, указанной пользователем

--- Fb2ToTxt/Program.cs
+++ Fb2ToTxt/Program.cs
@@ -1,6 +1,10 @@
-﻿namespace Fb2ToTxt;
+﻿using System.IO.Compression;
+
+namespace Fb2ToTxt;
 public static class Program
 {
+
+    private const string tempDirectoryName = "tempdirectoryfb2totxt";
     [STAThread]
     public static void Main()
     {
@@ -14,34 +18,40 @@ public static class Program
             if (dialog.ShowDialog() == DialogResult.OK)
             {
                 Console.WriteLine("Файл выбран, обработка файла.");
-                List<string> file = File.ReadAllLines(dialog.FileName).ToList();
-                for (int i = 0; i < file.Count; i++)
+                var fb2Book = dialog.FileName;
+                if (IOHelper.CheckSignature(dialog.FileName, 4, IOHelper.SignatureZip))
                 {
-                    while (file[i].Contains("<") && file[i].Contains(">"))
+                    Console.WriteLine("Файл является архивом. Требуется разархивировать его перед обработкой.");
+                    if (Directory.Exists(tempDirectoryName))
                     {
-                        var a = file[i].IndexOf("<");
-                        var b = file[i].IndexOf(">") + 1;
-                        if (a < b)
-                        {
-                            file[i] = file[i].Remove(a, b - a);
-                        }
-                        else
-                        {
-                            Console.WriteLine($"В строке {i + 1} возможна ошибка в обработке, так как > следует перед <");
-                        }
+                        Directory.Delete(tempDirectoryName, true);
                     }
-                    if (file[i].Trim() == string.Empty)
+                    Directory.CreateDirectory(tempDirectoryName);
+                    ZipFile.ExtractToDirectory(dialog.FileName, tempDirectoryName);
+                    foreach (var f in Directory.GetFiles(tempDirectoryName).Where(f => f.Contains(".fb2")))
                     {
-                        file.RemoveAt(i);
-                        i--;
+                        fb2Book = f;
                     }
                 }
+
+
+                List<string> file = File.ReadAllLines(fb2Book).ToList();
+                file = ProcessFileContents(file);
                 Console.WriteLine("Обработка завершена, переход к сохранению файла.");
-                var dir = Directory.CreateDirectory("export");
-                var fileName = dir.FullName + $"/filtered {dialog.SafeFileName}.txt";
-                File.WriteAllLines(fileName, file);
-                Console.WriteLine("Файл сохранен: " + fileName);
-                ExploreFile(fileName);
+
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Текстовый файл (*.txt)|*.txt";
+                saveFileDialog.FileName = $"Filtered [{dialog.SafeFileName}].txt";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllLines(saveFileDialog.FileName, file);
+                    Console.WriteLine("Файл сохранен: " + saveFileDialog.FileName);
+                    ExploreFile(saveFileDialog.FileName);
+                }
+                else
+                {
+                    Console.WriteLine("Сохранение файла отменено. Завершение работы программы.");
+                }
             }
             else
             {
@@ -50,35 +60,70 @@ public static class Program
         }
         catch (Exception ex)
         {
-            var dir = Directory.CreateDirectory("crashreports");
-            var crashFileName = dir.FullName + $"/crash-{DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss")}.txt";
-
-            string crashReport = $"При выполнении программы возникла ошибка: {ex.Message}\n\n. " +
-                $"Трассировка стека: {ex.StackTrace}\n\n";
-            if (ex.InnerException is not null)
+            GenerateCrashReport(ex);
+        }
+        finally
+        {
+            if (Directory.Exists(tempDirectoryName))
             {
-                crashReport+=$"Внутреннее исключение (при наличии): {ex.InnerException}";
+                Directory.Delete(tempDirectoryName, true);
             }
-
-            File.WriteAllText(crashFileName, crashReport);
-
-            Console.WriteLine($"При выполнении программы возникла ошибка: {ex.Message}. " +
-                $"\nПодробная информация сохранена в файле {crashFileName}");
-            Console.WriteLine("\nДля продолжения нажмите любую клавишу.");
-            Console.ReadKey();
         }
     }
 
-    public static bool ExploreFile(string filePath)
+    private static List<string> ProcessFileContents(List<string> file)
     {
-        if (!File.Exists(filePath))
+        for (int i = 0; i < file.Count; i++)
         {
-            return false;
+            while (file[i].Contains('<') && file[i].Contains('>'))
+            {
+                var a = file[i].IndexOf("<");
+                var b = file[i].IndexOf(">") + 1;
+                if (a < b)
+                {
+                    file[i] = file[i].Remove(a, b - a);
+                }
+                else
+                {
+                    Console.WriteLine($"В строке {i + 1} возможна ошибка в обработке, так как > следует перед <");
+                }
+            }
+            if (file[i].Trim() == string.Empty)
+            {
+                file.RemoveAt(i);
+                i--;
+            }
         }
-        //Clean up file path so it can be navigated OK
-        filePath = Path.GetFullPath(filePath);
-        System.Diagnostics.Process.Start("explorer.exe", string.Format("/select,\"{0}\"", filePath));
-        return true;
+
+        return file;
+    }
+
+    private static void GenerateCrashReport(Exception ex)
+    {
+        var dir = Directory.CreateDirectory("crashreports");
+        var crashFileName = dir.FullName + $"/crash-{DateTime.Now:yyyy-MM-dd HH-mm-ss}.txt";
+
+        string crashReport = $"При выполнении программы возникла ошибка: {ex.Message}\n\n. " +
+            $"Трассировка стека: {ex.StackTrace}\n\n";
+        if (ex.InnerException is not null)
+        {
+            crashReport += $"Внутреннее исключение (при наличии): {ex.InnerException}";
+        }
+
+        File.WriteAllText(crashFileName, crashReport);
+
+        Console.WriteLine($"При выполнении программы возникла ошибка: {ex.Message}. " +
+            $"\nПодробная информация сохранена в файле {crashFileName}");
+        Console.WriteLine("\nДля продолжения нажмите любую клавишу.");
+        Console.ReadKey();
+    }
+
+    private static void ExploreFile(string filePath)
+    {
+        if (File.Exists(filePath))
+        {
+            filePath = Path.GetFullPath(filePath);
+            System.Diagnostics.Process.Start("explorer.exe", string.Format("/select,\"{0}\"", filePath));
+        }
     }
 }
-
